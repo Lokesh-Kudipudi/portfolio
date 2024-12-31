56d814838cc908bc348ddab92b477662ced7007a
Lokesh_Kudipudi <lokeshkudipudi2006@gmail.com>
2024-12-31 10:54:22 +0530

    Uses invert for logos

--- src/Components/Skill.jsx
+++ src/Components/Skill.jsx
@@ -1,8 +1,12 @@
-function Skill({ imagePath, text }) {
+function Skill({ imagePath, text, invert }) {
   return (
-    <div className="skill flex h-[150px] w-[125px] flex-col items-center gap-2 rounded-2xl border-4 border-dashed border-black from-[#ffdfb4a4] to-[#95aabbae] p-6 shadow-xl md:h-[250px] md:w-[200px] dark:border-black dark:bg-white dark:bg-gradient-to-br">
-      <img src={imagePath} alt="html"></img>
-      <h1 className="text-center font-mono text-lg text-black dark:text-black">
+    <div className="skill flex h-[150px] w-[125px] flex-col items-center gap-2 rounded-2xl border-4 border-dashed border-black from-[#ffdfb4a4] to-[#95aabbae] p-6 shadow-xl md:h-[250px] md:w-[200px] dark:border-white">
+      <img
+        src={imagePath}
+        alt="html"
+        className={invert ? "dark:invert" : ""}
+      ></img>
+      <h1 className="text-center font-mono text-lg text-black dark:text-white">
         {text}
       </h1>
     </div>
--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -12,24 +12,30 @@ function Skills() {
         onMouseOut={(e) => handleHover(e, 1)}
         className="skills grid h-[65vh] grid-cols-2 gap-x-4 gap-y-3 overflow-scroll p-6 sm:grid-cols-3 md:grid-cols-4 md:gap-x-12 md:gap-y-6"
       >
-        <Skill imagePath="nextjs.svg" text="NextJs"></Skill>
-        <Skill imagePath="react.svg" text="ReactJS"></Skill>
-        <Skill imagePath="tailwind-css.svg" text="Tailwind"></Skill>
+        <Skill imagePath="nextjs.svg" text="NextJs" invert={true}></Skill>
+        <Skill imagePath="react.svg" text="ReactJS" invert={false}></Skill>
+        <Skill
+          imagePath="tailwind-css.svg"
+          text="Tailwind"
+          invert={false}
+        ></Skill>
         <Skill
           imagePath="socketio.svg"
           className="h-[150px]"
           text="SocketIO"
+          invert={true}
         ></Skill>
-        <Skill imagePath="nodejs.svg" text="NodeJS"></Skill>
-        <Skill imagePath="mongodb.svg" text="MongoDB"></Skill>
+        <Skill imagePath="nodejs.svg" text="NodeJS" invert={false}></Skill>
+        <Skill imagePath="mongodb.svg" text="MongoDB" invert={false}></Skill>
         <Skill
           imagePath="supabase.svg"
           className="h-[240px]"
           text="Supabase"
+          invert={false}
         ></Skill>
-        <Skill imagePath="html.svg" text="HTML"></Skill>
-        <Skill imagePath="css.svg" text="CSS"></Skill>
-        <Skill imagePath="js.svg" text="Javascript"></Skill>
+        <Skill imagePath="html.svg" text="HTML" invert={false}></Skill>
+        <Skill imagePath="css.svg" text="CSS" invert={false}></Skill>
+        <Skill imagePath="js.svg" text="Javascript" invert={false}></Skill>
       </div>
     </section>
   );
