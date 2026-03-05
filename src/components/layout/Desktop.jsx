e9ab4d9bc7e98b6092328131451313fe02dc7dc5
Lokesh-Kudipudi <lokeshkudipudi2006@gmail.com>
2026-03-05 14:05:55 +0530

    changed resume and added bg

--- src/components/layout/Desktop.jsx
+++ src/components/layout/Desktop.jsx
@@ -16,9 +16,7 @@ const Desktop = () => {
       animate={{ opacity: 1 }}
       transition={{ duration: 1 }}
       style={{
-        backgroundImage:
-          "url(https://images.unsplash.com/photo-1477346611705-65d1883cee1e?q=80&w=3270&auto=format&fit=crop)",
-      }}
+        backgroundImage: "url(/bg.avif)" }}
     >
       <TopBar />
 
